ceab024ecb032ee0d76307a55f6e8f7e1d0a0c8f
Angel Zamorano <angel.zamorano@innova-ti.cl>
2025-08-05 01:28:18 -0400

    Termino del desafio

---
+++ app.js
@@ -0,0 +1,20 @@
+// con el famoso DOM document objet model puedo tomar valores de las etiqutas en html para poder modificar datos
+
+let titulo = document.querySelector('h1'); // selecciono el h1 
+titulo.innerHTML = 'Juego del numero secreto'; // cambio el texto del tag h1 para que pueda visualizar
+
+let parrafo = document.querySelector('p'); // selecciono el p
+parrafo.innerHTML = 'Indica un numero del 1 al 10'; // cambio el texto del tag p para que pueda visualizar
+
+// vamos con el evento onclick de la primera funciona
+// vamos a agregar una funcion desde onclick edesde el html
+
+
+function intentoDeusuario(){// la funcion esta siendo llamada desde html
+    
+}
+
+
+// importante ver el siguiente link
+// https://www.aluracursos.com/blog/html-css-javascript-cuales-son-las-diferencias
+
---
+++ desafio/desafio.js
@@ -0,0 +1,34 @@
+
+
+// DESAFIO
+
+// 1 Cambia el contenido de la etiqueta h1 con document.querySelector y asigna el siguiente texto:
+//  "Hora del Desafío".
+let h1 = document.querySelector('h1');
+h1.innerHTML = 'Hora del Desafío';
+// 2 Crea una función que muestre en la consola el mensaje
+//  "El botón fue clicado" siempre que se presione el botón "Console".
+function mensajeConsola() {
+    alert('El boton fue clicado');   
+}
+
+// 3 Crea una función que se ejecute cuando se haga clic en el botón "prompt",
+//  preguntando el nombre de una ciudad de Brasil. Luego, muestra una alerta 
+// con el mensaje concatenando la respuesta con el texto: "Estuve en {ciudad} y me acordé de ti".
+function mensaje(params) {
+    let ciudad = prompt('¿Cuál es tu ciudad de Brasil?');
+    alert(`Estuve en ${ciudad} y me acordé de ti`)
+}
+
+// 4 Crea una función que muestre una alerta con el 
+// mensaje: "Yo amo JS" siempre que se presione el botón "Alerta".
+function alerta() {
+    alert("Yo amo JS");
+}
+
+// 5 Al hacer clic en el botón "suma", pide 2 números y muestra el resultado de la suma en una alerta.
+function suma(){
+    let num1 = parseInt(prompt('Ingrese el primer número'));
+    let num2 = parseInt(prompt('Ingrese el segundo número'));
+    alert(num1 + num2);
+}
